21d02fe6f1b1e23d3f526ecdc5f351828f43c519
NameText-c <nametext-c@outlook.com>
2025-01-03 16:29:05 +0800

    Optimized the cleaning order

--- main.bat
+++ main.bat
@@ -41,24 +41,25 @@ goto homePage
 title Cleaning - NCleaning
 mode con cols=150 lines=40
 cls
-:: ----------------------------------------
-del /f /s /q "%systemdrive%\*.tmp"
-del /f /s /q "%systemdrive%\*.temp"
-del /f /s /q "%systemdrive%\*.old"
-del /f /s /q "%systemdrive%\*.bak"
-del /f /s /q "%systemdrive%\*.log"
-del /f /s /q "%systemdrive%\*.gid"
-del /f /s /q "%systemdrive%\*.chk"
-:: ----------------------------------------
 del /f /s /q "%systemdrive%\temp\*.*"
 del /f /s /q "%windir%\temp\*.*"
 del /f /s/ q "%temp%\*.*"
 del /f /s /q "%userprofile%\local settings\temp\*.*"
 :: ----------------------------------------
 del /f /s /q "%windir%\SoftwareDistribution\Download\*.*"
+del /f /s /q "%systemdrive%\recycled\*.*"
 del /f /s /q "%userprofile%\cookies\*.*"
 del /f /s /q "%userprofile%\recent\*.*"
 del /f /s /q "%userprofile%\local settings\temporary internet files\*.*"
+:: ----------------------------------------
+del /f /s /q "%systemdrive%\*.tmp"
+del /f /s /q "%systemdrive%\*.temp"
+del /f /s /q "%systemdrive%\*.old"
+del /f /s /q "%systemdrive%\*.bak"
+del /f /s /q "%systemdrive%\*.log"
+del /f /s /q "%systemdrive%\*xlog"
+del /f /s /q "%systemdrive%\*.gid"
+del /f /s /q "%systemdrive%\*.chk"
 
 goto finishPage
 
