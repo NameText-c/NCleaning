ee40b5ef188a550ad5e84f365d3394b8b0f858ab
NameText-c <nametext-c@outlook.com>
2025-01-04 09:24:55 +0800

    Fixed bug clean files
    %systemdrive%\*xlog => %systemdrive\*.xlog

--- main.bat
+++ main.bat
@@ -58,7 +58,7 @@ del /f /s /q "%systemdrive%\*.temp"
 del /f /s /q "%systemdrive%\*.old"
 del /f /s /q "%systemdrive%\*.bak"
 del /f /s /q "%systemdrive%\*.log"
-del /f /s /q "%systemdrive%\*xlog"
+del /f /s /q "%systemdrive%\*.xlog"
 del /f /s /q "%systemdrive%\*.gid"
 del /f /s /q "%systemdrive%\*.chk"
 
