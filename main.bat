2097e21bbe93a1c0806cd2a23a83b5f8e3dd93e2
NameText-c <nametext-c@outlook.com>
2025-01-04 09:27:16 +0800

    Fixed bug UI
    Del a space

--- main.bat
+++ main.bat
@@ -41,12 +41,12 @@ goto homePage
 title Cleaning - NCleaning
 mode con cols=150 lines=40
 cls
-del /f /s /q "%systemdrive%\Windows.old\"
 del /f /s /q "%systemdrive%\temp\*.*"
 del /f /s /q "%windir%\temp\*.*"
 del /f /s/ q "%temp%\*.*"
 del /f /s /q "%userprofile%\local settings\temp\*.*"
 :: ----------------------------------------
+del /f /s /q "%systemdrive%\Windows.old\"
 del /f /s /q "%windir%\SoftwareDistribution\Download\*.*"
 del /f /s /q "%systemdrive%\recycled\*.*"
 del /f /s /q "%userprofile%\cookies\*.*"
@@ -73,6 +73,6 @@ echo               NCleaning / Clean
 echo.
 echo               Cleaning finished.             
 echo.
-echo    Press any key  to go back to home page.   
+echo    Press any key to go back to home page.    
 pause > nul
 goto homePage
